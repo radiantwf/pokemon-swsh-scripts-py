321b607cc427c2ebcc0db30064d4bbd678040e40
wangfeng <wangfeng@jyinform.com>
2020-03-26 16:24:36 +0800

    取蛋 孵蛋 脚本移值

---
+++ micropython/pokemon/get_eggs.py
@@ -0,0 +1,43 @@
+import poke_swsh_common, datetime
+from time import sleep
+
+def getEggs(delay=3):
+    times = 0
+    uart = poke_swsh_common.uart()
+    try:
+        poke_swsh_common.delay(uart,second)
+        while True:
+            # 人物移动
+            poke_swsh_common.send(uart,'LX MAX', 3)
+            sleep(0.5)
+            poke_swsh_common.send(uart,'LX MIN', 3.07)
+            sleep(0.5)
+            # 调整角度
+            poke_swsh_common.send(uart,'LY MIN', 0.1)
+            sleep(0.5)
+
+            # 取蛋
+            poke_swsh_common.send(uart,'Button A', 0.1)
+            sleep(0.8)
+            if times % 8 == 4:
+                # 拒蛋
+                poke_swsh_common.send(uart,'Button B', 0.1)
+                sleep(0.8)
+                poke_swsh_common.send(uart,'Button B', 0.1)
+                sleep(0.5)
+                poke_swsh_common.send(uart,'Button B', 0.1)
+            else:
+                poke_swsh_common.send(uart,'Button A', 0.1)
+                sleep(0.8)
+                delay = 0
+                while delay < 8:
+                    poke_swsh_common.send(uart,'Button B', 0.1)
+                    sleep(0.9)
+                    delay = delay + 1
+            sleep(1)
+            times = times + 1
+            if times % 10 == 0:
+                print("[%s] 脚本运行中，已执行了%d次取蛋动作" % (datetime.now(),times))
+    except KeyboardInterrupt:
+        poke_swsh_common.send(uart,'RELEASE')
+    print("[%s] 脚本运行结束，共执行了%d次取蛋动作" % (datetime.now(),times))
