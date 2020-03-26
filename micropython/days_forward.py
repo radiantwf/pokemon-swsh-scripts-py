e76fc6a2eb717c84e815b2bb5812ca6f4a2a58b7
wangfeng <wangfeng@jyinform.com>
2020-03-26 14:49:00 +0800

    调整结构

--- micropython/days_forward.py
+++ micropython/days_forward.py
@@ -1,11 +1,11 @@
 import poke_swsh_common, datetime
 
-def daysForwart(frames=1000):
+def daysForwart(frames=1000,second=3.0):
     times = 0
     day = 1
     uart = poke_swsh_common.uart()
     try:
-        poke_swsh_common.delay(uart)
+        poke_swsh_common.delay(uart,second)
 
         while(True):
             if times >= frames:
@@ -19,18 +19,21 @@ def daysForwart(frames=1000):
                 poke_swsh_common.gotoDatetimeSettingFromHome(uart)
             
             poke_swsh_common.initialAddOneDay(uart)
-            times += 1
             day += 1
             if day > 30:
                 day = 1
-            for num in range(0,1000):
+            else:
+                times += 1
+
+            for num in range(0,999):
                 if times >= frames:
                     break
                 poke_swsh_common.followingAddOneDay(uart)
-                times += 1
                 day += 1
                 if day > 30:
                     day = 1
+                else:
+                    times += 1
 
         print("[%s] 任务完成，已经跳过%d帧" % (datetime.now(),times))
     except KeyboardInterrupt:
--- micropython/raid.py
+++ micropython/raid.py
@@ -1,87 +1,19 @@
-from machine import UART
-from time import sleep
-import datetime
-
-def send(uart, msg, duration=0):
-    uart.write("%s\r\n"%(msg))
-    sleep(duration)
-    uart.write(b'RELEASE\r\n')
-    print("[%s] %s" % (datetime.now(),msg))
+import poke_swsh_common, datetime
 
 def raid(delay=3):
-    uart = UART(2, baudrate=9600)
-    print("[%s] %d秒延时（--delay参数设定）" % (datetime.now(),delay))
-    send(uart,'Button LCLICK', 0.1)
-    sleep(delay)
-    send(uart,'Button LCLICK', 0.1)
-    sleep(1)
-    print("[%s] 启动脚本" % (datetime.now()))
-
-    times = 0
+    uart = poke_swsh_common.uart()
     try:
+        poke_swsh_common.delay(uart,second)
+        times = 0
         while True:
-            # 启动游戏
-            send(uart,'Button A', 0.1)
-            sleep(1.5)
-            send(uart,'Button A', 0.1)
-            sleep(15)
-            send(uart,'Button A', 0.1)
-            sleep(6.5)
-
-            # 连接网络
-            send(uart,'Button Y', 0.1)
-            sleep(1)
-            send(uart,'Button START', 0.1)
-            sleep(30)
-            send(uart,'Button A', 0.1)
-            sleep(1)
-            send(uart,'Button B', 0.1)
-            sleep(1)
-
-            # 团战
-            send(uart,'Button A', 0.1)
-            sleep(8)
-            send(uart,'Button START', 0.1)
-            sleep(1)
-            # 设置密码2233
-            send(uart,'LX MAX', 0.1)
-            sleep(0.5)
-            send(uart,'Button A', 0.1)
-            sleep(0.4)
-            send(uart,'Button A', 0.1)
-            sleep(0.4)
-            send(uart,'LX MAX', 0.1)
-            sleep(0.5)
-            send(uart,'Button A', 0.1)
-            sleep(0.4)
-            send(uart,'Button A', 0.1)
-            sleep(0.4)
-            send(uart,'Button START', 0.1)
-            sleep(1)
-            send(uart,'Button A', 0.1)
-            sleep(1)
-
-            # 大家一起挑战
-            send(uart,'Button A', 0.1)
-            sleep(10)
-            send(uart,'LY Min', 0.1)
-            sleep(90)
-
-            # 开始
-            for num in range(0,20):
-                send(uart,'Button A', 0.1)
-                sleep(1)
-
-            # 关闭游戏
-            send(uart,'Button HOME', 0.1)
-            sleep(1.5)
-            send(uart,'Button X', 0.1)
-            sleep(0.5)
-            send(uart,'Button A', 0.1)
-            sleep(5)
-
+            poke_swsh_common.openGame(uart)
+            poke_swsh_common.online(uart)
+            poke_swsh_common.gotoRaid(uart)
+            poke_swsh_common.setRaidPassword(uart)
+            poke_swsh_common.waitForRaid(uart)
+            poke_swsh_common.raid(uart)
+            poke_swsh_common.closeGame(uart)
             times = times + 1
             print("[%s] 脚本运行中，已开启了%d次团战" % (datetime.now(),times))
-            
     except KeyboardInterrupt:
-        send(uart,'RELEASE')
+        poke_swsh_common.send(uart,'RELEASE')
