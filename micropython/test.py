6a4aac1dd7b2137b61b430be322d128cf1ed001a
wangfeng <wangfeng@jyinform.com>
2020-03-26 16:30:15 +0800

    脚本测试demo添加

--- micropython/test.py
+++ micropython/test.py
@@ -10,5 +10,11 @@ three_days_forward.threeDaysForward()
 import three_days_forward_raid
 three_days_forward_raid.threeDaysForwardAndRaid()
 
+import get_eggs
+get_eggs.getEggs()
+
+import hatching_eggs
+hatching_eggs.hatchingEggs(delay=3,initCol=0,maxBox=0,maxCol=5,eggCycle=20,flamebody=True)
+
 import machine
 machine.reset()
