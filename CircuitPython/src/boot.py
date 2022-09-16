142b4d1c84c71722531263f890fc84b48d7e5eee
wangfeng <wangfeng@jyinform.com>
2022-09-16 18:53:34 +0800

    ntp

--- CircuitPython/src/boot.py
+++ CircuitPython/src/boot.py
@@ -2,6 +2,7 @@ import usb_hid
 import hid.device.hori
 
 usb_hid.enable((hid.device.hori.HoriPadS,))
-import wifi
-wifi.radio.connect("WangFeng","radiantwf")
+
 # wifi.radio.connect("NETGERR-JY","19840618")
+wifi.radio.connect("WangFeng","radiantwf")
+print(customize.datetime.ntpSync())
